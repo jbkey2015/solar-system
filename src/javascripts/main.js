120c43644217fbbeddc198e7bc5622d3c9fd9f2a
Jbkey1983 <jbk_1_tn@yahoo.com>
2019-11-06 08:06:09 -0600

    added searchBar

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -3,12 +3,15 @@ import '../styles/main.scss';
 
 import list from './components/planetCard/planetCard';
 import singleCard from './components/singlePlanetCard/singlePlanetCard';
+import search from './components/searchBar/searchBar';
+
 
 const init = () => {
   list.createPlanetCard();
   list.hoverPlanets();
   singleCard.clickForSingleCard();
   singleCard.closePlanetCard();
+  search.searchFilter();
 };
 
 init();
